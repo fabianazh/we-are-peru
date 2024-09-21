95a69e8d129479f1cb5917994ef093ca4d3cda39
Muhammad Fabian Azhar <149347287+fabianazh@users.noreply.github.com>
2024-09-21 14:03:43 +0700

    Optimizing SEO

--- src/app/gallery/[galleryId]/page.tsx
+++ src/app/gallery/[galleryId]/page.tsx
@@ -46,9 +46,7 @@ export async function generateMetadata({
                 title: `${gallery?.title} | Galeri Kelompok Peru`,
                 description:
                     'Kelompok Peru adalah kelompok mahasiswa yang penuh semangat dan tekad untuk meraih kesuksesan bersama. Sebagai bagian dari MABIM (Masa Bimbingan) Universitas Nusa Putra, kelompok kami terbentuk dari berbagai latar belakang dan keahlian, namun disatukan oleh tujuan yang sama: belajar, berkolaborasi, dan saling mendukung dalam perjalanan akademik dan kehidupan kampus.',
-                images: gallery?.photos
-                    ? [gallery?.thumbnail, ...gallery?.photos]
-                    : [gallery?.thumbnail],
+                images: gallery?.photos,
             },
         }
     } catch (error) {
